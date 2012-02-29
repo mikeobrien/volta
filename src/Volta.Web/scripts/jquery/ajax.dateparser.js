ba2acf20aaeafe6739f5b52159d98868035abece
mob <mob@mikeobrien.net>
2012-02-28 23:45:21 -0500

    Added the ajax date parser.

---
+++ src/Volta.Web/scripts/jquery/ajax.dateparser.js
@@ -0,0 +1,24 @@
+﻿define(['jquery'], function($) {
+    $.ajaxPrefilter(function(ajax) {
+        var success = ajax.success;
+
+        var parseValue = function(name, value) {
+            if (typeof value === 'string' && !value.search(/^\/Date\(\d+\)\/$/))
+                return new Date(parseInt(value.substr(6)));
+            else return value;
+        };
+
+         var parseObject = function(obj) {
+            for (key in obj) {
+                var value = obj[key];
+                if (typeof value === 'object') parseObject(value);
+                else if (typeof value != 'function') obj[key] = parseValue(key, value);
+            }
+        };
+
+        ajax.success = function(data) {
+            parseObject(data);
+            success(data);
+        };
+    });
+});
